25bcdb4c1357f955d803a59b385a0ca2beac7938
MyName <dragon-negativ@yandex.ru>
2024-12-04 21:32:51 +0300

    first commit

---
+++ apps/blog/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Post, Category, Comment, Rating
+from mptt.admin import DraggableMPTTAdmin
+from django_mptt_admin.admin import DjangoMpttAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели записей
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdminPage(DjangoMpttAdmin):
+    """
+    Админ-панель модели комментариев
+    """
+    pass
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели рейтинга
+    """
+    pass
